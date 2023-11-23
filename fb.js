e3ffdba9452d6aa9cdcfece9afcbbf5bbba11260
xfshahrukh44 <ssidduit@gmail.com>
2023-11-24 02:57:10 +0500

    path

--- fb.js
+++ fb.js
@@ -11,7 +11,8 @@ export const getInfo = async (urls) => {
     const browser = await puppeteer.launch({
         headless: "new",
         // executablePath: path.join(__dirname, '/../../', '/home/servicedemoweb/.cache/puppeteer/chrome/linux-119.0.6045.105/chrome-linux64/')
-        executablePath: path.join(__dirname, '../..', '/.cache/puppeteer/chrome/linux-119.0.6045.105/chrome-linux64/')
+        executablePath: path.join(__dirname, '../..', '/.cache/puppeteer/chrome/linux-119.0.6045.105/chrome-linux64'),
+        args: [ "--no-sandbox", "--disable-setuid-sandbox" ]
     });
     // const browser = await puppeteer.launch({
     //     headless: false,
