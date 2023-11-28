9526b52c5ad09d8b64581768892638285b6a40ee
xfshahrukh44 <ssidduit@gmail.com>
2023-11-29 03:15:40 +0500

    puppeteer dir

--- fb.js
+++ fb.js
@@ -31,7 +31,7 @@ export const getInfo = async (urls) => {
 
     for (const url of urls) {
         await page.goto(url);
-        // await timeout(400);
+        await timeout(400);
 
         results.push({
             url,
