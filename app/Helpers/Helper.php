3463636c644186991bcf15502d21c5e1eb61f51c
xfshahrukh44 <ssidduit@gmail.com>
2023-12-01 00:08:12 +0500

    fb pages

--- app/Helpers/Helper.php
+++ app/Helpers/Helper.php
@@ -8,7 +8,7 @@ function get_page_info ($urls) {
         $curl = curl_init();
 
         curl_setopt_array($curl, array(
-            CURLOPT_URL => 'http://localhost:3044',
+            CURLOPT_URL => env('SCRAPER_URL'),
             CURLOPT_RETURNTRANSFER => true,
             CURLOPT_ENCODING => '',
             CURLOPT_MAXREDIRS => 10,
